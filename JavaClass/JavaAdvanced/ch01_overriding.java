74bf5eb2ec6c2c37e7f12fff7e9acab4f5114e95
Bijou <140521784+pyominmin@users.noreply.github.com>
2023-11-12 21:20:30 +0900

    Update ch01_overriding.java

--- JavaClass/JavaAdvanced/ch01_overriding.java
+++ JavaClass/JavaAdvanced/ch01_overriding.java
@@ -54,4 +54,96 @@ public class _01_Reference {
 
 2. Polymorphism
 //부모 클래스의 참조 변수로 자식 인스턴스를 참조할 수 있다.
+//오버로딩 : 메서드의 이름은 같지만 매개변수의 타입이나 개수가 다르면 다른 매서드로 간주한다.
+//오버라이딩 : 메서드의 원형이 완벽히 같은 메서드의 정의 부분을 재정의 한다. => 상속에서 사용 부모클래스의 대부분의 기능이 일치하는 경우 일치하지 않은 기능만 재정의 해서 사용할 수 있다.
+
+
+class Rectangle{
+	//instance method : 멤버함수
+	public Rectangle(){
+		System.out.println("1. 부모 생성자");
+	}
 	
+	public Rectangle(double width, double length){
+		mWidth = width;
+		mLength = length;
+		System.out.println("2. 부모 생성자");
+	}
+	
+	public Rectangle(Rectangle other){
+		mWidth = other.mWidth;
+		mLength = other.mLength;
+		System.out.println("3. 부모 복사 생성자");
+	}
+	
+	//gtter : instance variable 값을 리턴
+	public double getWidth() {
+		return mWidth;
+	}
+	public double getLength() {
+		return mLength;
+	}
+	//setter : instance variable의 값을 변경
+	public void setWidth(double width) {
+		mWidth = width;
+	}
+	public void setLength(double length) {
+		mLength = length; 
+	}
+	
+	//instance variable : 멤버변수
+	private double mWidth = 0, mLength = 0;//가로 세로
+}
+//-------------------------------------------------------------------------//Box클래스
+class Box extends Rectangle{//Rectangle 상속
+	public Box(){
+		//자식 클래스의 생성자에서는 부모 생성자를 호출한다.
+		//super();//부모 생성자를 호출
+		System.out.println("1. 자식 생성자");
+	}
+	public Box(double width, double length, double height){
+		//자식 클래스의 생성자에서는 부모 생성자를 호출한다.
+		//super();//명시적으로 호출하지 않을 시 내부적으로 호출되는 생성자 명령
+		super(width, length);//부모 생성자를 호출
+		mHeight = height;
+		System.out.println("2. 자식 생성자");
+	}
+	
+	public double getHeight() {
+		return mHeight;
+	}
+	
+	public void setHeight(double height) {
+		mHeight = height;
+	}
+	
+	public Box(Box ref){
+		super(ref);
+		mHeight = ref.mHeight;
+		System.out.println("3. 자식 복사 생성자");
+	}
+	
+	private double mHeight = 0;
+}
+
+
+//-------------------------------------------------------------------------//main
+public class _02_Polymorphism {
+
+	public static void main(String[] args) {
+		Box b1 = new Box();//인스턴스 생성 -> 생성자 호출
+		Box b2 = new Box(3, 4, 5);//인스턴스 생성
+		Box b3 = new Box(b2); 
+		Box b4 = null;
+		b4 = b3;
+		
+		Rectangle r1 = null;
+		r1 = b3;//자식 참조형 변수는 부모에 대입 가능
+		//부모의 멤버에만 접근 가능(자식 고유의 멤버에는 접근 불가능)
+		
+		System.out.println("주소값 : "+ System.identityHashCode(b2) +", r1 width : " + b2.getWidth() + ", length : " + b2.getLength() + ", height" + b2.getHeight());
+	}
+
+}
+
+3. overriding
