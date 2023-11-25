a52f5062aef78b27e466ab3e327d9089b358ea9e
Bijou <140521784+pyominmin@users.noreply.github.com>
2023-11-25 15:28:33 +0900

    Update ch09_IO_문자단위.java

--- JavaClass/JavaAdvanced/ch09_IO_문자단위.java
+++ JavaClass/JavaAdvanced/ch09_IO_문자단위.java
@@ -1,6 +1,6 @@
 *IO 문자단위
 
-1. readLine
+1. FileReader(readLine)
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -35,3 +35,30 @@ public class _08_FileReader {
 
 }
 
+2. FileWriter
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class _09_FileWriter {
+
+	public static void main(String[] args) {
+		//파일 기록 시 파일이 존재하지 않는 경우 파일을 생성해서 기록을 시작
+		try (FileWriter fw = new FileWriter("output.txt");
+				BufferedWriter bw = new BufferedWriter(fw)){
+			
+			String str = "FileWriter and BufferedWriter";
+			bw.write(str);
+			
+			System.out.println("output.txt파일에 기록을 완료했습니다.");
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
+2-1 FileWriter
